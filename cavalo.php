d92b509fbd28852b1b34aa985a9d1425833312e8
joao <joao@gmail.com>
2018-11-05 13:29:42 -0300

    Edicoes e novos arquivos prontos para usar em ambiente de producao

---
+++ cavalo.php
@@ -0,0 +1,12 @@
+<?php  require_once 'animal.php'  ?>
+
+<?php
+
+
+class Cavalo extends Animal {
+
+    
+    private $crina; 
+
+}?>
+
---
+++ ponei.php
@@ -0,0 +1,12 @@
+<?php  require_once 'cavalo.php'  ?>
+
+
+<?php
+
+
+class Ponei extends Cavalo {
+
+    
+    private $tamanho; 
+
+}?>
